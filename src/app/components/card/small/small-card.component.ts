e051c923f21e15e9d03f0c3ca2e7ed27ef2bc367
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2024-01-06 16:57:54 -0300

    fix: remove unnecessary import

--- src/app/components/card/small/small-card.component.ts
+++ src/app/components/card/small/small-card.component.ts
@@ -1,12 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { BlogPost } from '../../../model/blog-post';
-import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'card-sm',
   templateUrl: 'small-card.component.html',
   styleUrls: ['./small-card.component.scss'],
-  imports: [NgFor],
   standalone: true,
 })
 export class SmallCardComponent {

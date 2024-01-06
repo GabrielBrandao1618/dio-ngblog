0f2e283f3d1ba74e5de56b41a76f68a3a6b0b964
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2024-01-06 16:45:19 -0300

    feat: add small card component

---
+++ src/app/components/card/small/small-card.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'card-sm',
+  templateUrl: 'small-card.component.html',
+  styleUrls: ['./small-card.component.scss'],
+  standalone: true,
+})
+export class SmallCardComponent {}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MenuTitleComponent } from '../../components/menu-title/menu-title.component';
 import { LargeCardComponent } from '../../components/card/large/large-card.component';
+import { SmallCardComponent } from '../../components/card/small/small-card.component';
 
 @Component({
   selector: 'home-page',
   templateUrl: './home.component.html',
-  imports: [MenuTitleComponent, LargeCardComponent],
+  imports: [MenuTitleComponent, LargeCardComponent, SmallCardComponent],
   standalone: true,
   styleUrls: ['./home.component.scss'],
 })

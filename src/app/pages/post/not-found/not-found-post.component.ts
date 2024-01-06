03691436a297768c7100e563359c9b28ccbf99e4
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2024-01-06 17:22:35 -0300

    improve: not found post styles

--- src/app/pages/post/not-found/not-found-post.component.ts
+++ src/app/pages/post/not-found/not-found-post.component.ts
@@ -3,5 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'not-found-post',
   templateUrl: './not-found-post.component.html',
+  standalone: true,
+  styleUrls: ['not-found-post.component.scss'],
 })
 export class NotFoundPostComponent {}

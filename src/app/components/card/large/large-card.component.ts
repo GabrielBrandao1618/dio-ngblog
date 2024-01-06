d013bb081cf7d7ace12df2be10b0122d9df79140
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2024-01-06 16:23:34 -0300

    feat: add large card

---
+++ src/app/components/card/large/large-card.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'card-lg',
+  templateUrl: './large-card.component.html',
+  standalone: true,
+  styleUrls: ['./large-card.component.scss'],
+})
+export class LargeCardComponent {}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { MenuTitleComponent } from '../../components/menu-title/menu-title.component';
+import { LargeCardComponent } from '../../components/card/large/large-card.component';
 
 @Component({
   selector: 'home-page',
   templateUrl: './home.component.html',
-  imports: [MenuTitleComponent],
+  imports: [MenuTitleComponent, LargeCardComponent],
   standalone: true,
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {}

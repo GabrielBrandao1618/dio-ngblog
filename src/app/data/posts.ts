b6dcc553295b694cdb15a8e94f0c17b6222b7bb0
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2024-01-06 14:49:35 -0300

    feat: add blog post model and example posts

---
+++ src/app/data/posts.ts
@@ -0,0 +1,16 @@
+import { BlogPost } from '../model/blog-post';
+
+export const posts: BlogPost[] = [
+  {
+    id: 1,
+    description: 'This is the first post',
+    photo: 'https://via.placeholder.com/600/e01d41',
+    title: 'First post!',
+  },
+  {
+    id: 2,
+    description: 'This is the second post',
+    photo: 'https://via.placeholder.com/600/621de0',
+    title: 'Second post!',
+  },
+];

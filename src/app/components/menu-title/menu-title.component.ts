c944a2345dcf6142f55cff982d90dcd1062e6ded
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2024-01-06 16:02:55 -0300

    feat: add menu title component

---
+++ src/app/components/menu-title/menu-title.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'menu-title',
+  templateUrl: './menu-title.component.html',
+  styleUrls: ['./menu-title.component.scss'],
+})
+export class MenuTitleComponent {}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { MenuTitleComponent } from '../../components/menu-title/menu-title.component';
 
 @Component({
   selector: 'home-page',
   templateUrl: './home.component.html',
+  imports: [MenuTitleComponent],
+  standalone: true,
 })
 export class HomeComponent {}

aad1efe581596dc288ada8dc55a78b48168f4d0c
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2024-01-06 17:03:37 -0300

    feat: add open post on post click feature

--- src/app/components/card/large/large-card.component.ts
+++ src/app/components/card/large/large-card.component.ts
@@ -1,11 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { BlogPost } from '../../../model/blog-post';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'card-lg',
   templateUrl: './large-card.component.html',
   standalone: true,
   styleUrls: ['./large-card.component.scss'],
+  imports: [RouterLink],
 })
 export class LargeCardComponent {
   @Input() post?: BlogPost;
--- src/app/components/card/small/small-card.component.ts
+++ src/app/components/card/small/small-card.component.ts
@@ -1,11 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { BlogPost } from '../../../model/blog-post';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'card-sm',
   templateUrl: 'small-card.component.html',
   styleUrls: ['./small-card.component.scss'],
   standalone: true,
+  imports: [RouterLink],
 })
 export class SmallCardComponent {
   @Input() post?: BlogPost;
